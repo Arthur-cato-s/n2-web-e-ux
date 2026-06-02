d07fd7cfcf9fedbf3c9f95ea6f471ea9f0a88a81
Arthur <arthur.signorelli@catolicasc.edu.br>
2026-06-01 21:24:31 -0300

    1.1

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-function buscarPostagens(){
- console.log ("oi")   
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(function (API_retorno) {
+        return API_retorno.json()
+    }).then(dados_tratados)
+
+function dados_tratados(coisa) {
+    const elementoPesquisado = document.getElementById('escreve-aqui')
+    for (let loop = 0; loop < coisa.length; loop++) {
+        // regra
+        const dado = coisa[loop];
+        elementoPesquisado.innerHTML += `
+         <article id="card_${dado.id}" class="card">
+         <h2> ${dado.title} </h2>
+         <div> ${dado.body} </div>
+         <div> <button onclick="deletar('card_${dado.id}')"> Deletar </button> </div>
+         </article>
+        `
+     // fim de regra
+    }
 }
+
+function deletar(id) {
+    const elementoQueVamosDeletar = document.getElementById(id)
+    elementoQueVamosDeletar.remove()
+}a
