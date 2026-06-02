801bcb94f86e5d6e07a83752662b08925aa360cf
Arthur <arthur.signorelli@catolicasc.edu.br>
2026-06-02 20:25:59 -0300

    teste 1.0

--- script_2.0.js
+++ script_2.0.js
@@ -6,20 +6,21 @@ fetch("https://jsonplaceholder.typicode.com/users?utm_source")
 function dados_tratados1(coisa1) {
     const elementoPesquisado = document.getElementById('escreve-aqui1')
     for (let loop = 0; loop < coisa1.length; loop++) {
-        // regra
         const dado1 = coisa1[loop];
         elementoPesquisado.innerHTML += `
          <article id="card1_${dado1.id}" class="card">
-         <h2> ${dado1.title} </h2>
-         <div> ${dado1.body} </div>
-         <div> <button onclick="deletar('card_${dado1.id}')"> Deletar </button> </div>
+         <h2> ${dado1.name} </h2>
+         <div> ${dado1.email} </div>
+         <div> ${dado1.company?.name || ""} </div>
+         <div> <button onclick="deletar('card1_${dado1.id}')"> Escolher </button> </div>
          </article>
         `
-     // fim de regra
     }
 }
 
 function deletar(id) {
     const elementoQueVamosDeletar = document.getElementById(id)
-    elementoQueVamosDeletar.remove()
-}a
+    if (elementoQueVamosDeletar) {
+        elementoQueVamosDeletar.remove()
+    }
+}
